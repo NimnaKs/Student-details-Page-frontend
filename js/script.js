39e5934193e4336f8cf85084bc4df750bc5983f1
NimnaKs <nimnakaveesha123456@gmail.com>
2023-12-26 21:24:46 +0530

    Add Student Details Forms

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ $(document).ready(function () {
         });
     });
 });
+
+function openAddStudentModal() {
+    $('#studentFormHeading').text('Add New Student');
+    $('#saveUpdateButton').text('Save');
+    $('#studentModal').modal('show');
+    $('#saveUpdateButton').removeClass('btn-warning');
+    $('#saveUpdateButton').addClass('btn-success');
+}
+
+// Function to open the modal for Update Student
+function openUpdateStudentModal() {
+    $('#studentFormHeading').text('Update Student');
+    $('#saveUpdateButton').text('Update');
+    $('#saveUpdateButton').removeClass('btn-success');
+    $('#saveUpdateButton').addClass('btn-warning');
+    $('#studentModal').modal('show');
+}
+
+$('#addBtn').on('click', openAddStudentModal);
+$('#updateBtn').on('click', openUpdateStudentModal);
