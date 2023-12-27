987d57d946dc2b9120b8f183c308a074e33c649c
NimnaKs <nimnakaveesha123456@gmail.com>
2023-12-27 20:27:03 +0530

    alerts and validation done

--- controller/studentController.js
+++ controller/studentController.js
@@ -17,6 +17,10 @@ let batchNo = $('#batchNo');
 
 let search = $('#searchInput');
 
+const emailPattern = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
+const mobilePattern = new RegExp("^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$");
+
+
 search.on("keyup", function () {
     let value = $(this).val().toLowerCase();
     $("tbody tr").filter(function () {
@@ -57,36 +61,47 @@ saveUpdateBtn.on('click', (event) => {
     let prog = program.val();
     let b_no = batchNo.val();
 
-    let studentModel = new StudentModel(
-        student_id,
-        f_name,
-        l_name,
-        contact_no,
-        email_add,
-        add,
-        prog,
-        b_no
-    );
+    if (
+        validation(f_name, "student first name", null) &&
+        validation(l_name, "student last name", null) &&
+        validation(contact_no, "student contact", mobilePattern.test(contact_no)) &&
+        validation(email_add, "student email", emailPattern.test(email_add)) &&
+        validation(add, "student addresss", null) &&
+        validation(prog, "student program", null) && 
+        validation(b_no, "student batch no", null) 
+        ){
 
-    if(saveUpdateBtn.text()==='Save'){
-        student_db.push(studentModel);
-    }else{
-        student_db.map((student) => {
-            if(studentModel.studentId === student.studentId){
-                student.fName = studentModel.fName;
-                student.lName = studentModel.lName;
-                student.contact = studentModel.contact;
-                student.email = studentModel.email;
-                student.address = studentModel.address;
-                student.program = studentModel.program;
-                student.batchNo = studentModel.batchNo;
-            }
-        });
+        let studentModel = new StudentModel(
+            student_id,
+            f_name,
+            l_name,
+            contact_no,
+            email_add,
+            add,
+            prog,
+            b_no
+        );
+
+        if (saveUpdateBtn.text() === 'Save') {
+            student_db.push(studentModel);
+        } else {
+            student_db.map((student) => {
+                if (studentModel.studentId === student.studentId) {
+                    student.fName = studentModel.fName;
+                    student.lName = studentModel.lName;
+                    student.contact = studentModel.contact;
+                    student.email = studentModel.email;
+                    student.address = studentModel.address;
+                    student.program = studentModel.program;
+                    student.batchNo = studentModel.batchNo;
+                }
+            });
+        }
+
+        clear.click();
+
+        populateStudentTable();
     }
-
-    clear.click();
-
-    populateStudentTable();
 
 });
 
@@ -124,12 +139,12 @@ function populateStudentTable() {
         );
     });
 
-    $('.updateBtn').on('click', function() {
+    $('.updateBtn').on('click', function () {
         const studentId = $(this).data('student-id');
-        openStudentModal('Update Student', 'Update', 'btn-warning',studentId);
+        openStudentModal('Update Student', 'Update', 'btn-warning', studentId);
     });
 
-    $('.deleteBtn').on('click', function() {
+    $('.deleteBtn').on('click', function () {
         const studentId = $(this).data('student-id');
         deleteStudent(studentId);
     });
@@ -138,17 +153,36 @@ function populateStudentTable() {
 function deleteStudent(studentId) {
 
     const index = student_db.findIndex(student => student.studentId === studentId);
-    
-    if (index !== -1) {
-        student_db.splice(index, 1);
-        populateStudentTable(); 
-    }
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Delete'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            if (index !== -1) {
+                student_db.splice(index, 1);
+                populateStudentTable();
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            } else {
+                showError('Error', 'Student cannot found !');
+            }
+        }
+    });
 
 }
 
-function openStudentModal(heading, buttonText, buttonClass,stuId) {
+function openStudentModal(heading, buttonText, buttonClass, stuId) {
 
-    if(stuId){
+    if (stuId) {
         const student = student_db.find(student => student.studentId === stuId);
         studentId.val(student.studentId);
         firstName.val(student.fName);
@@ -165,4 +199,28 @@ function openStudentModal(heading, buttonText, buttonClass,stuId) {
     $('#studentModal').modal('show');
     saveUpdateBtn.removeClass('btn-success btn-warning').addClass(buttonClass);
 
+}
+
+function showError(title, text) {
+    Swal.fire({
+        icon: 'error',
+        title: title,
+        text: text,
+        footer: '<a href="">Why do I have this issue?</a>'
+    });
+}
+
+function validation(value, message, test) {
+    if (!value) {
+        showError('Null Input', 'Input ' + message);
+        return false;
+    }
+    if (test === null) {
+        return true;
+    }
+    if (!test) {
+        showError('Invalid Input', 'Invalid Input ' + message);
+        return false;
+    }
+    return true;
 }
